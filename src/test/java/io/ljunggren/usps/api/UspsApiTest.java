3f8935145115a95a7b63b1440a38b9b148f86afa
aljunggren <aljunggren@DML-32TQYD3.digitalmanagement.net>
2022-02-15 14:24:38 -0500

    Updated mockito, lombok, and junit versions

--- src/test/java/io/ljunggren/usps/api/UspsApiTest.java
+++ src/test/java/io/ljunggren/usps/api/UspsApiTest.java
@@ -3,7 +3,7 @@ package io.ljunggren.usps.api;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import java.io.ByteArrayInputStream;
@@ -20,7 +20,7 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import io.ljunggren.usps.api.tracking.response.ErrorResponse;
 import io.ljunggren.usps.api.tracking.response.TrackingResponse;
